b1057488a9c6fb5a7ec1a493c938860d7bf81237
Keith Winfield <keithwinfield@Keiths-MacBook-Pro.local>
2020-04-08 18:21:46 -0400

    Made template Layouts

---
+++ resources/views/test.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="welcome-jumbo">
+        <div class="status">NEW</div>
+        <h1>Billy Burger</h1>
+        <img src="img/hamburger-and-fries-png-4.png" class="burger-fries">
+    </div>
+@endsection
