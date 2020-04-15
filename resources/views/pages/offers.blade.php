2b465f7e262e51959457cdddb2c16a26dc06a410
Keith Winfield <keithwinfield@Keiths-MacBook-Pro.local>
2020-04-15 10:23:13 -0400

    Offers and Menu Index Pages Added

--- resources/views/pages/offers.blade.php
+++ resources/views/pages/offers.blade.php
@@ -2,7 +2,57 @@
 
 
 @section('content')
-    <div class="">
-        <h1>Offers</h1>
+    <div id="offers-page">
+        <div class="content-box">
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+                    <h2>Sign Up To Receive Offers</h2>
+                    <form>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="firstnameinput">Email Address</label>
+                                    <input type="fname" class="form-control" id="firstnameinput" placeholder="John">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="lastnameinput">Phone Number</label>
+                                    <input type="lname" class="form-control" id="lastnameinput" placeholder="Doe">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="emailInput">Email Address</label>
+                                    <input type="email" class="form-control" id="emailInput" placeholder="name@example.com">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="phoneInput">Phone Number</label>
+                                    <input type="phone" class="form-control" id="phoneInput" placeholder="000-000-0000">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <p>
+                                    In signing up I acknowledge that I am 18 years of age or older, want to receive email offers from Billys Burgers and, if I select to join Dine Rewards, agree to the terrms and conditions of the program.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                 <button type="submit" class="btn btn-primary mb-2">Confirm</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
