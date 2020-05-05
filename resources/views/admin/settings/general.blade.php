90e70f470be14bd8c1ca4f4b4de5cf9ab85f92f6
Keith Winfield <keithwinfield@Keiths-Mac-Pro.rem.remichel.com>
2020-05-05 16:05:31 -0400

    Setup General Settings update

--- resources/views/admin/settings/general.blade.php
+++ resources/views/admin/settings/general.blade.php
@@ -10,14 +10,14 @@
                 <div class="row">
                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                         <div class="page-header">
-                            <h2 class="pageheader-title">New Food Category </h2>
+                            <h2 class="pageheader-site_title">General Settings</h2>
                             <p class="pageheader-text">Proin placerat ante duiullam scelerisque a velit ac porta, fusce sit amet vestibulum mi. Morbi lobortis pulvinar quam.</p>
                             <div class="page-breadcrumb">
                                 <nav aria-label="breadcrumb">
                                     <ol class="breadcrumb">
                                         <li class="breadcrumb-item"><a href="/admin" class="breadcrumb-link">Dashboard</a></li>
-                                        <li class="breadcrumb-item"><a href="/admin/food-categories" class="breadcrumb-link">All Food Category</a></li>
-                                        <li class="breadcrumb-item active" aria-current="page">New Food Category</li>
+                                        <li class="breadcrumb-item"><a href="/admin/food-categories" class="breadcrumb-link">All General Settings</a></li>
+                                        <li class="breadcrumb-item active" aria-current="page">Edit General Settings</li>
                                     </ol>
                                 </nav>
                             </div>
@@ -29,34 +29,69 @@
                 <!-- ============================================================== -->
                 <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
                                 <div class="card">
-                                        <h5 class="card-header">Create a New Category</h5>
+                                        <h5 class="card-header">Edit General Settings</h5>
                                         <div class="card-body">
-                                        <form class="splash-container" method="POST" action="/admin/food-categories">
+                                        <form class="splash-container" method="POST" action="/admin/settings/general">
                                                 @csrf
+                                                @method('PUT')
                                                 <div class="form-group">
-                                                    <label for="inputtitle">Title</label>
-                                                    <input id="inputtitle" type="text" class="form-control form-control-lg @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" required autocomplete="title" autofocus placeholder="Give Category a Title">
-                                                    @error('title')
+                                                    <label for="inputtitle">Business Name</label>
+                                                    <input id="inputtitle" type="text" class="form-control form-control-lg @error('site_title') is-invalid @enderror" name="site_title" value="{{ old('site_title', $category->site_title) }}" required autocomplete="site_title" autofocus placeholder="Add Site Title">
+                                                    @error('site_title')
                                                         <span class="invalid-feedback" role="alert">
                                                             <strong>{{ $message }}</strong>
                                                         </span>
                                                     @enderror
                                                 </div>
                                                 <div class="form-group">
-                                                    <label for="inputimageurl">Image Url</label>
-                                                    <textarea id="inputimageurl" type="text" class="form-control form-control-lg @error('image_url') is-invalid @enderror" name="image_url"  required autofocus placeholder="Add image url">{{ old('image_url') }}</textarea>
-
-                                                    @error('image_url')
+                                                    <label for="inputaddress1">Address 1</label>
+                                                    <input id="inputaddress1" type="text" class="form-control form-control-lg @error('address_1') is-invalid @enderror" name="address_1" value="{{ old('address_1', $category->address_1) }}" required autocomplete="address_1" autofocus placeholder="Add Street Address">
+                                                    @error('address_1')
                                                         <span class="invalid-feedback" role="alert">
                                                             <strong>{{ $message }}</strong>
                                                         </span>
                                                     @enderror
                                                 </div>
                                                 <div class="form-group">
-                                                    <label for="inputdescription">Description</label>
-                                                    <textarea id="inputdescription" type="text" class="form-control form-control-lg @error('description') is-invalid @enderror" name="description" required autofocus placeholder="Write a Description">{{ old('description') }}</textarea>
-
-                                                    @error('description')
+                                                    <label for="inputaddress2">Address 1</label>
+                                                    <input id="inputaddress2" type="text" class="form-control form-control-lg @error('address_2') is-invalid @enderror" name="address_2" value="{{ old('address_2', $category->address_2) }}" autocomplete="address_2" autofocus placeholder="">
+                                                    @error('address_2')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="inputcity">City</label>
+                                                    <input id="inputcity" type="text" class="form-control form-control-lg @error('city') is-invalid @enderror" name="city" value="{{ old('city', $category->city) }}" required autocomplete="city" autofocus placeholder="Add City">
+                                                    @error('city')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="inputstate">State</label>
+                                                    <input id="inputstate" type="text" class="form-control form-control-lg @error('state') is-invalid @enderror" name="state" value="{{ old('state', $category->state) }}" required autocomplete="state" autofocus placeholder="Add State">
+                                                    @error('state')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="inputzipcode">Zipcode</label>
+                                                    <input id="inputzipcode" type="text" class="form-control form-control-lg @error('zipcode') is-invalid @enderror" name="zipcode" value="{{ old('zipcode', $category->zipcode) }}" required autocomplete="zipcode" autofocus placeholder="Add Zipcode">
+                                                    @error('zipcode')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="inputphonenumber">Phone Number</label>
+                                                    <input id="inputphonenumber" type="text" class="form-control form-control-lg @error('phone_number') is-invalid @enderror" name="phone_number" value="{{ old('phone_number', $category->phone_number) }}" required autocomplete="phone_number" autofocus placeholder="Add Phone Number">
+                                                    @error('phone_number')
                                                         <span class="invalid-feedback" role="alert">
                                                             <strong>{{ $message }}</strong>
                                                         </span>
--- routes/web.php
+++ routes/web.php
@@ -106,17 +106,17 @@ Route::get('/admin/reservations/{id}/delete', 'admin\ReservationController@delet
 
 // Admin Settings
 
-Route::get('/admin/settings/general', 'admin\SettingsController@general');
+Route::get('/admin/settings/general', 'admin\SettingController@general');
 
-Route::post('/admin/settings/general', 'admin\SettingsController@saveGeneral');
+Route::put('/admin/settings/general', 'admin\SettingController@saveGeneral');
 
-Route::get('/admin/settings/seo', 'admin\SettingsController@seo');
+Route::get('/admin/settings/seo', 'admin\SettingController@seo');
 
-Route::post('/admin/settings/seo', 'admin\SettingsController@saveSeo');
+Route::put('/admin/settings/seo', 'admin\SettingController@saveSeo');
 
-Route::get('/admin/settings/social', 'admin\SettingsController@social');
+Route::get('/admin/settings/social', 'admin\SettingController@social');
 
-Route::post('/admin/settings/social', 'admin\SettingsController@saveSocial');
+Route::put('/admin/settings/social', 'admin\SettingController@saveSocial');
 
 // Admin Users
 Route::get('/admin/users', 'admin\UsersController@index');
