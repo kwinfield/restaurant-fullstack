aaffe4af818d33fd7a53007718bfd4f5fec1e6a4
Keith Winfield <keithwinfield@Keiths-Mac-Pro.rem.remichel.com>
2020-05-14 13:08:25 -0400

    Adjusted Migrations

--- database/migrations/2020_04_27_190410_create_role_user_table.php
+++ database/migrations/2020_04_27_190410_create_role_user_table.php
@@ -14,7 +14,7 @@ class CreateRoleUserTable extends Migration
     public function up()
     {
         Schema::create('role_user', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id')->unsigned();
             $table->bigInteger('role_id')->unsigned()->index();
             $table->bigInteger('user_id')->unsigned()->index();
             $table->timestamps();
