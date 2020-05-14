e4b617969e484312c423810386f25958cd1ed442
Keith Winfield <keithwinfield@Keiths-Mac-Pro.rem.remichel.com>
2020-05-14 12:57:51 -0400

    Adjusted Migrations

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,7 +62,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \App\Http\Middleware\Role::class,
-        'cors' => \Fruitcake\Cors\CorsServiceProvider::class,
     ];
 
     /**
